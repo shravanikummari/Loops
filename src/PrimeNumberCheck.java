a61d630c51f9899f737618e709070e1c067b4b17
shravanikummari <shravanikummari1234@gmail.com>
2024-06-30 19:43:37 -0700

    Added PrimeNumberCheck class successfully

---
+++ src/PrimeNumberCheck.java
@@ -0,0 +1,18 @@
+public class PrimeNumberCheck {
+        public static boolean isPrime(int n) {
+            if (n <= 1) {
+                return false;
+            }
+            for (int i = 2; i <= Math.sqrt(n); i++) {
+                if (n % i == 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        public static void main(String[] args) {
+            int number = 29;
+            System.out.println("Is " + number + " a prime number? " + isPrime(number));
+        }
+    }
